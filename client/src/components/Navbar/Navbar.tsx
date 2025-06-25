804e7bcf081f6072008812fbb81fbafb6baf88f5
unlikelyUsual <kumarprabhat308@gmail.com>
2025-06-25 21:51:29 +0530

    Client : Navbar fix

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -111,6 +111,7 @@ const Navbar: React.FC = () => {
           <ul className="py-2" aria-labelledby="user-menu-button">
             <li>
               <Link
+                onClick={() => setIsDropdownOpen(false)}
                 to="/dashboard"
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
               >
@@ -119,6 +120,7 @@ const Navbar: React.FC = () => {
             </li>
             <li>
               <Link
+                onClick={() => setIsDropdownOpen(false)}
                 to="/profile"
                 className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
               >
